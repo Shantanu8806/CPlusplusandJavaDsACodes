5818b9a36730c59ec971b0baa4e5f98bd5ddfb14
Shantanu8806 <119732714+Shantanu8806@users.noreply.github.com>
2023-08-29 14:20:32 +0530

    Create Dfs1.java

---
+++ JavaDsaCodes/Dfs1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void dfs(int node,boolean vis[],ArrayList<ArrayList<Integer>> adj,ArrayList<Integer>ls)
+    {
+        vis[node]=true;
+        ls.add(node);
+        
+        //getting neighbours
+        for(Integer it:adj.get(node))
+        {
+            if(vis[it]==false)
+            {
+                dfs(it,vis,adj,ls);
+            }
+        }
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean vis[]=new boolean[V+1];
+        ArrayList<Integer>ls=new ArrayList<Integer>();
+        dfs(0,vis,adj,ls);
+        
+        return ls;
+    }
+}
