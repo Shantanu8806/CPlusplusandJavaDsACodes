ab4466e4bcc81220d208a83cc9191a2af175be95
Shantanu8806 <119732714+Shantanu8806@users.noreply.github.com>
2023-09-02 16:06:03 +0530

    Create FirstUniqueCharacterInAString.java

---
+++ JavaDsaCodes/FirstUniqueCharacterInAString.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(freq[s.charAt(i)-'a']==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+/*
+OR
+
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq=new int[26];
+        char[] chars=s.toCharArray();
+        for(char c:chars)
+        {
+           freq[c-'a']++;
+        }
+        for(int i=0;i<chars.length;i++)
+        {
+            if(freq[chars[i]-'a']==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
+*/
