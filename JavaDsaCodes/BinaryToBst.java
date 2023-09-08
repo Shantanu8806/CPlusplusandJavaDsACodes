b041ae856683400208542cf67113a22e280a135a
Shantanu8806 <119732714+Shantanu8806@users.noreply.github.com>
2023-09-09 00:05:18 +0530

    Create BinaryToBst.java

---
+++ JavaDsaCodes/BinaryToBst.java
@@ -0,0 +1,27 @@
+class Solution
+{
+    // The given root is the root of the Binary Tree
+    // Return the root of the generated BST
+    Node binaryTreeToBST(Node root)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        Collections.sort(list);
+        binaryTreeToBST(root,list);
+        return root;
+    }
+    public void inorder( Node root, ArrayList<Integer> list ){
+        if(root!=null) {
+            inorder(root.left, list);
+            list.add(root.data);
+            inorder(root.right, list);
+        }
+    }
+    public static void binaryTreeToBST( Node root, ArrayList<Integer> list ){
+        if(root!=null) {
+            binaryTreeToBST(root.left, list);
+            root.data = list.remove(0);
+            binaryTreeToBST(root.right, list);
+        }
+    }
+}
