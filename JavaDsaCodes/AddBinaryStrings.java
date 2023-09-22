37aa2585bee6b7c90ad2fcc934b4c768eb9f0360
Shantanu8806 <119732714+Shantanu8806@users.noreply.github.com>
2023-09-22 20:01:13 +0530

    Create AddBinaryStrings.java

---
+++ JavaDsaCodes/AddBinaryStrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int n=a.length()-1;
+        int m=b.length()-1;
+
+        StringBuilder sb = new StringBuilder();
+        int carry=0;
+
+        while(n>=0 || m>=0 || carry==1)
+        {
+            if(n>=0)
+            {
+                carry+= a.charAt(n--)-'0';
+            }
+            if(m>=0)
+            {
+                carry+=b.charAt(m--)-'0';
+            }
+            sb.append(carry%2);
+            carry /= 2;
+        }
+       return sb.reverse().toString();
+    }
+}
